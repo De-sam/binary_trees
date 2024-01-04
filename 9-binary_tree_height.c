285d55e9da4ed7de74921c8f6e1dec7930710ebb
De-sam <samsoncoded@gmail.com>
2024-01-04 16:51:42 +0100

    hight of node

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,5 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 
-	return (1 + (left_height > right_height ? left_height : right_height));
+	if (left_height > right_height)
+		return (1 + left_height);
+	else
+		return (1 + right_height);
 }
